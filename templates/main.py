307e21cb49ba1878cc864fa41f0e7c3238358aa5
Istalantar <istalantar@gmail.com>
2021-12-04 13:26:38 +0100

    added main.py template

---
+++ templates/main.py
@@ -0,0 +1,20 @@
+from myFunctions import my_input
+
+
+def main():
+    content = my_input("input.txt")
+
+    part_one(content)
+    part_two(content)
+
+
+def part_one(content):
+    pass
+
+
+def part_two(content):
+    pass
+
+
+if __name__ == '__main__':
+    main()
