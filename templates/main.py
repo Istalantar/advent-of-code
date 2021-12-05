288763e30457ad1d0eed6cfc6466df9d4419b3b8
Istalantar <istalantar@gmail.com>
2021-12-05 10:56:24 +0100

    adds unittests to template

--- templates/main.py
+++ templates/main.py
@@ -1,19 +1,33 @@
 from myFunctions import my_input
+from unittest import TestCase
+
+
+class TestExample(TestCase):
+    def setUp(self) -> None:
+        self.content = my_input("example.txt")
+
+    def test_part_one(self):
+        self.assertEqual(part_one(self.content), -1)
+
+    def test_part_two(self):
+        self.assertEqual(part_two(self.content), -1)
 
 
 def main():
     content = my_input("input.txt")
 
-    part_one(content)
-    part_two(content)
+    print(part_one(content))
+    print(part_two(content))
 
 
-def part_one(content):
-    pass
+def part_one(content) -> int:
+    res = 0
+    return res
 
 
-def part_two(content):
-    pass
+def part_two(content) -> int:
+    res = 0
+    return res
 
 
 if __name__ == '__main__':
