0d0dfa9664d9b9c4021e4c5725f06342edf25fe0
Istalantar <istalantar@gmail.com>
2021-12-13 21:54:04 +0100

    2021 Day 12 Part 1

---
+++ 2021/Day_12/main.py
@@ -0,0 +1,67 @@
+import sys
+
+sys.path.append('../..')
+from myFunctions import my_input_list  # noqa E402
+
+
+def main():
+    content = my_input_list("input.txt")
+
+    print(part_one(content))
+    print(part_two(content))
+
+
+def part_one(content) -> int:
+    caves = set(sum([line.split('-') for line in content], []))
+    caves_dict = {}
+
+    # get all caves with adjacent caves -> {'cave': ['adj1', 'adj2', ...]}
+    for cave in caves:
+        connected_caves = []
+        for line in content:
+            if cave + '-' in line or '-' + cave in line:
+                if (temp := str(line).replace(cave, '').replace('-', '')) != 'start':
+                    connected_caves.append(temp)
+        caves_dict[cave] = connected_caves
+
+    # find not allowed caves, e.g. small cave, which would need to be visited twice
+    not_allowed_caves = []
+    for key, val in caves_dict.copy().items():
+        if len(val) <= 1 and val[0].islower():
+            not_allowed_caves.append(key)
+            caves_dict.pop(key)
+
+    # remove not allowed caves
+    for key, val in caves_dict.copy().items():
+        for cave in not_allowed_caves:
+            val.remove(cave) if cave in val else False
+
+    # get unique ways
+    visited = []
+    output_list = []
+    output = ['start']
+    trace('start', output_list, output, visited, caves_dict)
+
+    return len(output_list)
+
+
+def part_two(content) -> int:
+    return 0
+
+
+def trace(node, output_list, output, visited, graph):
+    if node == 'end':
+        output_list.append(output.copy())
+
+    for cave in graph[node]:
+        if (cave not in visited or cave.isupper()) and 'end' not in output:
+            visited.append(cave) if cave.islower() else False
+            output.append(cave)
+            trace(cave, output_list, output, visited, graph)
+            popped = output.pop(-1) if output else False
+            if popped in visited:
+                visited.remove(popped)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2021/Day_12/test_main.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from main import part_one, part_two
+import sys
+
+sys.path.append('../..')
+from myFunctions import my_input_list  # noqa E402
+
+
+class TestExample(TestCase):
+    def setUp(self) -> None:
+        self.content = my_input_list("example.txt")
+        self.content_2 = my_input_list("example2.txt")
+        self.content_3 = my_input_list("example3.txt")
+
+    def test_part_one1(self):
+        self.assertEqual(10, part_one(self.content))
+
+    def test_part_one2(self):
+        self.assertEqual(19, part_one(self.content_2))
+
+    def test_part_one3(self):
+        self.assertEqual(226, part_one(self.content_3))
+
+    def test_part_two(self):
+        self.assertEqual(103, part_two(self.content_2))
+
+    def test_part_two2(self):
+        self.assertEqual(3509, part_two(self.content_2))
