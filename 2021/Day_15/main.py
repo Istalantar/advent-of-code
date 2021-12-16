8e3343f581df83ef2e8f81116f5d22b2c5ea1264
Istalantar <istalantar@gmail.com>
2021-12-16 21:54:13 +0100

    2021 Day 15 Part 2 continuation

--- 2021/Day_15/main.py
+++ 2021/Day_15/main.py
@@ -36,15 +36,14 @@ def part_one(content) -> int:
 
 # noinspection PyTypeChecker
 def part_two(content) -> int:
-    max_y = len(content) - 1
-    max_x = len(content[0]) - 1
+    max_y = len(content)
+    max_x = len(content[0])
     cave = [[int(i) for i in row] for row in content]
 
     risk_graph = []
-    unvisited = set()
-    for y in range(max_y + 1):
+    for y in range(max_y):
         row = []
-        for x in range(max_x + 1):
+        for x in range(max_x):
             row.append(None)
         risk_graph.append(row)
 
@@ -58,7 +57,7 @@ def part_two(content) -> int:
         for x in range(len(cave[0])):
             cave[y][x] = cave[y][x] + i
             cave[y][x] -= 9 if cave[y][x] > 9 else 0
-            i += 1 if x % (max_x + 1) == 9 else 0
+            i += 1 if x % max_x == 9 else 0
 
     # adjust risk in y direction
     temp_cave = copy.deepcopy(cave)
@@ -72,13 +71,13 @@ def part_two(content) -> int:
         for x in range(len(cave[0])):
             cave[y][x] = cave[y][x] + i
             cave[y][x] -= 9 if cave[y][x] > 9 else 0
-        i += 1 if y % (max_y + 1) == 9 else 0
+        i += 1 if y % max_y == 9 else 0
 
     unvisited = {(x, y) for x in range(len(cave[0])) for y in range(len(cave))}
 
     # run path search
     risk_graph[0][0] = 0  # starting point
-    my_dijkstra(cave, risk_graph, unvisited)
+    my_dijkstra2(cave, risk_graph, unvisited)
 
     return risk_graph[len(cave) - 1][len(cave[0]) - 1]
 
@@ -102,6 +101,54 @@ def my_dijkstra(cave, risk_graph, unvisited):
                         risk_graph[y + 1][x] = risk + cave[y + 1][x]
 
                 unvisited.remove((x, y))
+
+
+def my_dijkstra2(cave, risks, unvisited):
+    x = 0
+    y = 0
+    current_node = 0
+
+    while unvisited:
+        # look into right neighbour
+        if not x + 1 >= len(risks[0]):
+            if risks[y][x + 1] is None:
+                risks[y][x + 1] = risks[y][x] + cave[y][x + 1]
+            elif risks[y][x + 1] > risks[y][x] + cave[y][x + 1]:
+                risks[y][x + 1] = risks[y][x] + cave[y][x + 1]
+
+        # look into below neighbour
+        if not y + 1 >= len(risks):
+            if risks[y + 1][x] is None:
+                risks[y + 1][x] = risks[y][x] + cave[y + 1][x]
+            elif risks[y + 1][x] > risks[y][x] + cave[y + 1][x]:
+                risks[y + 1][x] = risks[y][x] + cave[y + 1][x]
+
+        # look into left neighbour
+        if not x - 1 < 0:
+            if risks[y][x - 1] is None:
+                risks[y][x - 1] = risks[y][x] + cave[y][x - 1]
+            elif risks[y][x - 1] > risks[y][x] + cave[y][x - 1]:
+                risks[y][x - 1] = risks[y][x] + cave[y][x - 1]
+
+        # look into upper neighbour
+        if not y - 1 < 0:
+            if risks[y - 1][x] is None:
+                risks[y - 1][x] = risks[y][x] + cave[y - 1][x]
+            elif risks[y - 1][x] > risks[y][x] + cave[y - 1][x]:
+                risks[y - 1][x] = risks[y][x] + cave[y - 1][x]
+
+        # ToDo: follow least risky path
+        # go right
+        if risks[y][x+1] < risks[y+1][x] and risks[y][x+1] < risks[y][x-1]:
+            pass
+
+        # go down
+
+        # go left
+
+        # go up
+
+        unvisited.remove((x, y))
 
 
 if __name__ == '__main__':
--- 2021/Day_15/test_main.py
+++ 2021/Day_15/test_main.py
@@ -9,9 +9,13 @@ from myFunctions import my_input_list  # noqa E402
 class TestExample(TestCase):
     def setUp(self) -> None:
         self.content = my_input_list("example.txt")
+        self.content2 = my_input_list("example_discord.txt")
 
     def test_part_one(self):
         self.assertEqual(40, part_one(self.content))
+
+    def test_part_one2(self):
+        self.assertEqual(29, part_one(self.content2))
 
     def test_part_two(self):
         self.assertEqual(315, part_two(self.content))
