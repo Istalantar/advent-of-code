01b18d854186e993a1118eceacd8d2178d8cbb1f
Istalantar <istalantar@gmail.com>
2021-12-17 11:32:04 +0100

    2021 Day 12 splits test case into to parts

--- 2021/Day_12/test_main.py
+++ 2021/Day_12/test_main.py
@@ -6,7 +6,7 @@ sys.path.append('../..')
 from myFunctions import my_input_list  # noqa E402
 
 
-class TestExample(TestCase):
+class TestPartOne(TestCase):
     def setUp(self) -> None:
         self.content = my_input_list("example.txt")
         self.content_2 = my_input_list("example2.txt")
@@ -21,8 +21,18 @@ class TestExample(TestCase):
     def test_part_one3(self):
         self.assertEqual(226, part_one(self.content_3))
 
+
+class TestPartTwo(TestCase):
+    def setUp(self) -> None:
+        self.content = my_input_list("example.txt")
+        self.content_2 = my_input_list("example2.txt")
+        self.content_3 = my_input_list("example3.txt")
+
     def test_part_two(self):
-        self.assertEqual(103, part_two(self.content_2))
+        self.assertEqual(36, part_two(self.content_2))
 
     def test_part_two2(self):
+        self.assertEqual(103, part_two(self.content_2))
+
+    def test_part_two3(self):
         self.assertEqual(3509, part_two(self.content_2))
