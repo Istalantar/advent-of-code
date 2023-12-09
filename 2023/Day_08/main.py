9bab02a1ff8062185d279384258ad2839d4aa99e
Istalantar <istalantar@gmail.com>
2023-12-09 01:54:01 +0100

    2023 Day 8 Part 2

--- 2023/Day_08/main.py
+++ 2023/Day_08/main.py
@@ -1,15 +1,18 @@
 import sys
 import itertools
+from math import lcm
+import logging
 
 sys.path.append('../..')
 from myFunctions import my_input_list  # noqa E402
 
 
 def main():
+    logging.basicConfig(level=logging.INFO, style='{', format='{message}')
     aoc_input = my_input_list("input.txt")
 
-    print(part_one(aoc_input.copy()))
-    print(part_two(aoc_input.copy()))
+    logging.info(part_one(aoc_input.copy()))
+    logging.info(part_two(aoc_input.copy()))
 
 
 def part_one(aoc_input) -> int:
@@ -48,24 +51,31 @@ def part_two(aoc_input) -> int:
         elif node_name[2] == 'Z':
             ending_nodes.add(node_name)
 
-    not_finished = True
-    i = 0
-    instructions = itertools.cycle(aoc_input[0])
-    current_nodes = starting_nodes
-    while not_finished:
-        i += 1
-        instruction = next(instructions)
-        next_nodes = set()
-        for node in current_nodes:
-            next_node = nodes[node][0] if instruction == 'L' else nodes[node][1]
-            next_nodes.add(next_node)
+    steps_to_end = {node: 0 for node in starting_nodes}
+    for node in starting_nodes:
+        logging.debug(f'Tracing {node=}')
+        not_finished = True
+        i = 0
+        instructions = itertools.cycle(aoc_input[0])
+        current_node = node
+        while not_finished:
+            i += 1
+            instruction = next(instructions)
+            next_node = nodes[current_node][0] if instruction == 'L' else nodes[current_node][1]
+            if next_node in ending_nodes:
+                not_finished = False
+            else:
+                current_node = next_node
+        steps_to_end[node] = i
+        logging.debug(f'Steps for {node} = {i}')
 
-        if all(node[2] == 'Z' for node in next_nodes):
-            not_finished = False
-        else:
-            current_nodes = next_nodes
-
-    return i
+    # after finding the first ending for each starting node,
+    # use 'least common multiple' to find the step where they all meet
+    # Example, to get the idea:
+    # person A can go around a track in 3 minutes and person B runs around the same track in 5 minutes.
+    # If they start at the same time, how long does it take for them to meet at the start again?
+    # lcm(3, 5) -> 15 Minutes
+    return lcm(*steps_to_end.values())
 
 
 if __name__ == '__main__':
